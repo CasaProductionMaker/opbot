e86bed0008c3be89a600178d3a3f12ef36334e3e
storm-let <a.new.mail.user@gmail.com>
2025-05-30 19:22:16 -0700

    Start update inventory format

--- util.js
+++ util.js
@@ -14,8 +14,8 @@ export function fillInProfileBlanks(profile) {
     if (!profile.xp) profile.xp = 0;
     profile.level = Math.floor(Math.sqrt(profile.xp) * 0.6);
     if (!profile.stars) profile.stars = 0;
-    if (!profile.inventory) profile.inventory = {"0_0": 1};
-    if (!profile.loadout) profile.loadout = ["0_0", "-1_0", "-1_0"];
+    if (!profile.inventory) profile.inventory = {"0": [0, 0, 0, 0, 0, 0, 0, 0, 0]};
+    if (!profile.loadout) profile.loadout = [0, -1, -1];
     if (!profile.health) profile.health = 30;
     let totalMH = 0;
     for(let i = 0; i < 3; i++) {
